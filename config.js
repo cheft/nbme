b650faac0b60ddf366a6b16c10f1d58ba9bf4de0
chenhaifeng314 <7720829@gmail.com>
2013-11-14 14:20:55 +0800

    demo use bootstrap v3

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     cookieSecret: 'nbme',
     connectionUrl: 'mongodb://localhost/nbme',
-    
+
     dev: true,
     jsList: [
         'lib/jquery/jquery-1.9.1.js',
@@ -9,9 +9,11 @@ module.exports = {
         'lib/handlebars/handlebars.js',
         'lib/ember/ember-1.0.0-rc.7.js',
         'lib/ember-data/ember-data-0.14.js',
-        'lib/bootstrap/js/bootstrap-2.3.1.js',
+        'lib/bootstrap/js/bootstrap.js',
         'app.js'
     ],
     cssList: [
+        'lib/bootstrap/css/bootstrap.min.css',
+        'lib/bootstrap/css/bootstrap-theme.min.css'
     ]
 };
